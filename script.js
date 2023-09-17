28a3023facc771dd8497d9508055b4566799a4ae
ytb <62708858+ytb664@users.noreply.github.com>
2023-09-17 13:09:03 +0700

    Create 16x16 grid
    with the row and column div

--- script.js
+++ script.js
@@ -4,14 +4,12 @@ for (let i = 0; i < 16; i++) {
 
     const row = document.createElement('div');
     row.classList.add('row');
-    row.textContent = "i";
     container.appendChild(row);
     
     for (let j = 0; j < 16; j++) {
 
-        const square = document.createElement('div');
-        square.classList.add('square');
-        square.textContent = `j: ${j}`;
-        container.appendChild(square);    
+        const column = document.createElement('div');
+        column.classList.add('column');
+        row.appendChild(column);    
     }
 }

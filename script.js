f40b6cfd2fed86c3541e771c8ce81263f6905388
ytb <62708858+ytb664@users.noreply.github.com>
2023-09-17 12:50:34 +0700

    Change square to row
    the variable and class so as to make it easier

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const container = document.querySelector('.container');
 
 for (let i = 0; i < 16; i++) {
 
+    const row = document.createElement('div');
+    row.classList.add('row');
+    row.textContent = "i";
+    container.appendChild(row);
+    
     for (let j = 0; j < 16; j++) {
 
         const square = document.createElement('div');
